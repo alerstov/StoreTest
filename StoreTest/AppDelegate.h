8dfa321950137f6cbcca37e816672a4c76aac2c0
Alexander Stepanov <alerstov@gmail.com>
2012-07-28 13:22:34 +0400

    Initial Commit

---
+++ StoreTest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  StoreTest
+//
+//  Created by Lion User on 28.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
